62f4b775fcda860acdb396cad9307d7b33fefe61
manuelecheandia <manuel.ep.10@gmail.com>
2021-01-28 22:26:15 -0500

    se actualiza app.js

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const app = express();
 const publicPath= path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
-
-app.listen(3000, () => {
-    console.log('Servidor corriendo en el puerto 3000')
-    });
+app.listen(process.env.PORT || 3000, function () {
+    console.log("server up");
+  });
+  
 
 app.get('/', (req,res) => {
     res.sendFile(path.resolve(__dirname,'./views/home.html'));
